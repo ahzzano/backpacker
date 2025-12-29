f65cca2ead780710ad67f30b58e50941c21096bb
W. Santos <w.santos.gg@gmail.com>
2025-12-29 16:34:28 +0800

    feat: add opening of backpacks

--- src/main/java/com/avery/backpacker/listeners/PlayerListener.java
+++ src/main/java/com/avery/backpacker/listeners/PlayerListener.java
@@ -4,18 +4,43 @@ import java.util.UUID;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
 
+import com.avery.backpacker.BackpackInventory;
 import com.avery.backpacker.BagManager;
 import com.avery.backpacker.Plugin;
 
 public class PlayerListener implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-            
+        Action action = event.getAction();
+        ItemStack currentItem = event.getItem();
+        if (currentItem == null) {
+            return;
+        }
+
+        boolean is_backpack = currentItem.getPersistentDataContainer().getOrDefault(Plugin.getInstance().backpack, PersistentDataType.BOOLEAN, false);
+        if (!is_backpack) {
+            return;
+        }
+        // Backpacking Time
+
+        if (action != Action.RIGHT_CLICK_AIR) {
+            return;
+        }
+
+        String bag_id = currentItem.getPersistentDataContainer().getOrDefault(Plugin.getInstance().backpack_id, PersistentDataType.STRING, null);
+        if (bag_id != null) {
+            // Open the bag
+            BackpackInventory inv = BagManager.getInstance().getBackpackInventory(bag_id);
+            event.getPlayer().openInventory(inv.getInventory());
+        }
+
+
     }
 
     @EventHandler
